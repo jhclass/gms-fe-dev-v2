1e21410dfd5f47e59322a96b178bbb50dc98157c
hyelim <otter4913@gmail.com>
2023-10-06 10:47:21 +0900

    Jest 모노레포

--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -1,18 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "@/pages/css/index.module.css";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import { faHeadset } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
 
-// import Swiper and modules styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,57 +39,6 @@ const HSecond = styled.div`
     padding: 0 15px;
   }
 `;
-const HGnb = styled.ul`
-  margin-left: 20px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  li {
-    padding: 0 15px;
-  }
-`;
-
-const MContents = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 50px 0;
-  flex-direction: column;
-`;
-const MContentsTitle = styled.h2`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const McontentsSubText = styled.p`
-  margin: 20px 0 50px;
-`;
-
-const GridContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 16px;
-  div {
-    background: transparent;
-    img {
-      width: 100%;
-      object-fit: cover;
-      vertical-align: bottom;
-    }
-    h3 {
-      font-size: 20px;
-      margin-top: 10px;
-      font-weight: 600;
-      padding: 0 5px 0;
-    }
-    p {
-      margin-top: 10px;
-      line-height: 1.6;
-      padding: 0 5px 0;
-    }
-  }
-`;
 const FootContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -115,29 +52,6 @@ const FInfo = styled.div`
   margin-left: 20px;
 `;
 
-const Aside = styled.div`
-  position: fixed;
-  bottom: 50px;
-  right: 10px;
-  div {
-    width: 60px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #007aff;
-    color: #fff;
-    border-radius: 30px;
-    margin-top: 10px;
-  }
-  div:first-child {
-    background-color: #333;
-  }
-`;
-const StyledIcon = styled(FontAwesomeIcon)`
-  width: 24px !important;
-  height: 24px !important;
-`;
 export default function Home() {
   return (
     <>
@@ -167,134 +81,8 @@ export default function Home() {
       <div>
         <Wrapper>
           <div>
-            <Swiper
-              pagination={{
-                dynamicBullets: true,
-              }}
-              modules={[Pagination]}
-              className="mySwiper1"
-            >
-              <SwiperSlide>
-                <img
-                  src="https://placehold.it/1440x600"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="https://placehold.it/1440x600"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="https://placehold.it/1440x600"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="https://placehold.it/1440x600"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="https://placehold.it/1440x600"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="https://placehold.it/1440x600"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://placehold.it/1440x600" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://placehold.it/1440x600" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://placehold.it/1440x600" />
-              </SwiperSlide>
-            </Swiper>
+            어드민민민민
           </div>
-          <MContents>
-            <MContentsTitle>현재 모집중이예요</MContentsTitle>
-            <McontentsSubText>지금 결제하면 20만원 추가 할인!</McontentsSubText>
-            <GridContainer>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-              <div>
-                <img
-                  src="https://placehold.it/500x500"
-                  style={{ borderRadius: 10, overflow: "hidden" }}
-                />
-                <h3>JAVASCRIPT 마스터 과정 (6개월)</h3>
-                <p>최신 자바스크립트 es6를 활용하여 풀스택개발에 도전하세요.</p>
-              </div>
-            </GridContainer>
-          </MContents>
-          <MContents>
-            <MContentsTitle>생생 리뷰 TALK!</MContentsTitle>
-            <McontentsSubText>지금 결제하면 20만원 추가 할인!</McontentsSubText>
-          </MContents>
         </Wrapper>
       </div>{" "}
       {/**content */}
@@ -336,15 +124,6 @@ export default function Home() {
           </FootContainer>
         </Wrapper>
       </footer>{" "}
-      {/**footer */}
-      <Aside>
-        <div>
-          <StyledIcon icon={faHeadset} />
-        </div>
-        <div>
-          <StyledIcon icon={faAngleUp} />
-        </div>
-      </Aside>{" "}
       {/**footer */}
     </>
   );
