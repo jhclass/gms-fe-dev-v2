4e00c508d60fd1ed2f980be3641bb1850d1074b1
hyelim <otter4913@gmail.com>
2023-10-17 19:44:17 +0900

    수정

--- packages/academy/tailwind.config.js
+++ packages/academy/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     screens: {
