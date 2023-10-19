439b6c7b751c097598edc920ede12f598a383d96
hyelim <otter4913@gmail.com>
2023-10-19 15:19:38 +0900

    네비수정

--- packages/academy/components/MenuAll.tsx
+++ packages/academy/components/MenuAll.tsx
@@ -1,7 +1,6 @@
 
 import { useState } from 'react';
 import MenuList from "@/components/MenuList";
-import { Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react';
 
 interface DropdownMenuProps {
   isOpen: boolean;
