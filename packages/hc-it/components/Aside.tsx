793012b8b38162c902e2417e399fedb3d5e72bea
hyelim <otter4913@gmail.com>
2023-10-26 16:22:02 +0900

    aside

--- packages/hc-it/components/Aside.tsx
+++ packages/hc-it/components/Aside.tsx
@@ -37,7 +37,7 @@ export default function Aside() {
   return (
     <>
       <aside id="aside" className={`${asideHidden ? 'opacity-0' : 'opacity-100'} ${moAsideOpen ? 'translate-x-0':'translate-x-[calc(100%+0.01rem)]'} bg-zinc-700/80 py-3 px-2 bottom-[5%] rounded-s-lg right-0 fixed z-50 lg:py-0 lg:px-0 lg:bg-transparent lg:right-16 xl:right-16 lg:bottom-28 transition-all`}>
-        <span onClick={handleAsideClick} className="cursor-pointer lg:hidden w-[2rem] h-[3rem] text-white text-4xl/none leading-[3.5rem] indent-[-0.05rem] rounded-s-lg bg-zinc-700/80 absolute top-[50%] mt-[-1.5rem] left-[-2rem]"><i className="xi-angle-left-min" /></span>
+        <span onClick={handleAsideClick} className="flex items-center cursor-pointer lg:hidden w-[2rem] h-[3rem] text-white text-4xl/none indent-[-0.05rem] rounded-s-lg bg-zinc-700/80 absolute top-[50%] mt-[-1.5rem] left-[-2rem]"><i className="xi-angle-left-min" /></span>
         <div className="flex items-center justify-center w-12 h-12 text-white rounded-full text-2xl/none bg-primary">
           <Link href="/consult" as="/consult">
             <i className="xi-call"/>
