80199f4c9157dfcd3b6cb27a96c124ded911d492
hyelim <otter4913@gmail.com>
2023-11-06 17:16:35 +0900

    섹션추가

---
+++ packages/hc-it/components/section/Section01._3col.tsx
@@ -0,0 +1,43 @@
+export default function Section01_3col() {
+  return (
+    <>
+      <div className="wrap">
+        <div className="grid gap-8 sm:grid-cols-1 lg:grid-cols-3">
+          <div className="py-10 bg-black rounded-lg">
+            <p className="mx-auto my-0 text-lg text-center">
+              <span className="text-black border-2 border-[#61DAFB] font-bold bg-[#61DAFB] rounded-lg px-3 py-2 mb-2">
+                01 Next.js로 할 수 있는 모든 것
+              </span>
+            </p>
+            <p className="mt-5 text-base text-center text-white">
+              Next.js의 기본/심화 기능을 익히고 <br />
+              완성도 높은 프로젝트를 구축부터 배포까지!
+            </p>
+          </div>
+          <div className="py-10 bg-black rounded-lg">
+            <p className="mx-auto my-0 text-lg text-center">
+              <span className="text-black border-2 border-[#61DAFB] font-bold bg-[#61DAFB] rounded-lg px-3 py-2 mb-2">
+                01 Next.js로 할 수 있는 모든 것
+              </span>
+            </p>
+            <p className="mt-5 text-base text-center text-white">
+              Next.js의 기본/심화 기능을 익히고 <br />
+              완성도 높은 프로젝트를 구축부터 배포까지!
+            </p>
+          </div>
+          <div className="py-10 bg-black rounded-lg">
+            <p className="mx-auto my-0 text-lg text-center">
+              <span className="text-black border-2 border-[#61DAFB] font-bold bg-[#61DAFB] rounded-lg px-3 py-2 mb-2">
+                01 Next.js로 할 수 있는 모든 것
+              </span>
+            </p>
+            <p className="mt-5 text-base text-center text-white">
+              Next.js의 기본/심화 기능을 익히고 <br />
+              완성도 높은 프로젝트를 구축부터 배포까지!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ packages/hc-it/components/section/Section01_img.tsx
@@ -0,0 +1,21 @@
+export default function Section01_img() {
+  return (
+    <>
+      <div className="wrap">
+        <p className="text-black">
+          <b className="inline-block mt-5 text-2xl">
+            Next.js 완전 정복
+            <br />
+            확장성 높은 커머스 서비스 구축하기
+          </b>
+        </p>
+        <figure className="w-full mt-10 overflow-hidden rounded-xl">
+          <img src="/src/images/MAYA_01.png" width="100%" />
+        </figure>
+        <figure className="inline-block w-full mt-10 overflow-hidden rounded-xl">
+          <img src="/src/images/MAYA_02.png" width="100%" />
+        </figure>
+      </div>
+    </>
+  )
+}
