ce413ca54b61a21631f068ed60771f177121936a
Jinhyoung Lee <jhcode0@naver.com>
2023-11-14 17:16:01 +0900

    authLink

--- packages/admin/lib/apolloClient.ts
+++ packages/admin/lib/apolloClient.ts
@@ -1,14 +1,37 @@
 // lib/apolloClient.ts
 import { useRouter } from 'next/router'
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  makeVar,
+  createHttpLink,
+  ApolloLink,
+} from '@apollo/client'
 
 console.log(process.env.NEXT_PUBLIC_GRAPHQL_URI, '환경변수')
 const TOKEN = 'token'
 export const isLoggedInVar = makeVar(
   typeof window !== 'undefined' ? Boolean(localStorage.getItem(TOKEN)) : false,
 )
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(TOKEN)
+  const headers = operation.getContext().headers || {} // 기본 헤더 객체 설정
+  console.log('authLink 안에토큰있음', token)
+  console.log(headers, '이건뭔데??')
+  operation.setContext({
+    headers: {
+      ...headers,
+      token: token,
+    },
+  })
+  return forward(operation)
+})
 export const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI, // GraphQL 서버의 엔드포인트를 입력합니다.
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
@@ -16,6 +39,8 @@ export const apolloClient = new ApolloClient({
 export const LogUserIn = (token: string) => {
   localStorage.setItem(TOKEN, token)
   isLoggedInVar(true)
+
+  console.log('로그인되었음', String(localStorage.getItem(TOKEN)))
 }
 
 //로그아웃
