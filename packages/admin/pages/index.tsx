96b767e496d733365fc263be09363b61c4dd5fd6
hyelim <otter4913@gmail.com>
2023-12-14 18:00:58 +0900

    대시보드작업중

--- packages/admin/pages/index.tsx
+++ packages/admin/pages/index.tsx
@@ -1,6 +1,11 @@
+import List from '@/components/common/List'
+import NewConsultNum from '@/components/dashboard/NewConsultNum'
+import ConsultNum from '@/components/dashboard/ConsultNum'
+import MainWrap from '@/components/wrappers/MainWrap'
 import Layout from '@/components/wrappers/MainWrap'
 import { navOpenState } from '@/lib/recoilAtoms'
-import { motion } from 'framer-motion'
+import { AnimatePresence, Reorder, motion } from 'framer-motion'
+import { useState } from 'react'
 import { useRecoilState } from 'recoil'
 import styled from 'styled-components'
 
@@ -16,13 +21,68 @@ const Wrap = styled(motion.div)<{ $navOpen: boolean }>`
     padding: 4rem 0 0 0;
   }
 `
-export default function Home() {
-  const [navOpen] = useRecoilState(navOpenState)
+const HomeArea = styled.div`
+  display: inline-grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+`
+const FirstArea = styled.div``
+const SecondArea = styled.div``
+const ThirdArea = styled.div``
+const FourthArea = styled.div``
 
+const DragBox = styled.div`
+  position: relative;
+  width: 300px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 24px;
+`
+const ItemBox = styled.div`
+  border-radius: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 15px 18px;
+  position: relative;
+  background: white;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  cursor: grab;
+`
+const initialItems = ['🍅 Tomato', '🥒 Cucumber', '🧀 Cheese', '🥬 Lettuce']
+export default function Home() {
+  const [items, setItems] = useState(initialItems)
   return (
     <>
       <Layout>
-        <p>대시보드</p>
+        <HomeArea>
+          <div>
+            <NewConsultNum />
+          </div>
+          <div>
+            <ConsultNum />
+          </div>
+          <div>
+            <DragBox>
+              <AnimatePresence initial={true}>
+                <Reorder.Group axis="y" values={items} onReorder={setItems}>
+                  {items.map((item, index) => (
+                    // <List key={index} item={item} />
+                    <Reorder.Item key={item} value={item}>
+                      <ItemBox>{item}</ItemBox>
+                    </Reorder.Item>
+                  ))}
+                </Reorder.Group>
+              </AnimatePresence>
+            </DragBox>
+          </div>
+        </HomeArea>
       </Layout>
     </>
   )
