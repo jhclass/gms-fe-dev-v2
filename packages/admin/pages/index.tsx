704a70faad5e9c1c410f3a1a75f472ed51fe5303
hyelim <otter4913@gmail.com>
2023-12-18 17:44:16 +0900

    git merge main

--- packages/admin/pages/index.tsx
+++ packages/admin/pages/index.tsx
@@ -19,8 +19,19 @@ const Wrap = styled(motion.div)<{ $navOpen: boolean }>`
 `
 const HomeArea = styled.div`
   display: inline-grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(4, minmax(320px, 1fr));
   gap: 1rem;
+
+  @media screen and (max-width: 1695px) {
+    grid-template-columns: repeat(3, minmax(320px, 1fr));
+  }
+
+  @media screen and (max-width: 1140px) {
+    grid-template-columns: repeat(2, minmax(320px, 1fr));
+  }
+  @media screen and (max-width: 720px) {
+    grid-template-columns: repeat(1, minmax(320px, 1fr));
+  }
 `
 
 export default function Home() {
