106f0f8b8864746cbf3ea463bc1c91290d51cf96
hyelim <otter4913@gmail.com>
2023-12-26 17:47:11 +0900

    subject수정

--- packages/admin/pages/consult/detail/[id].tsx
+++ packages/admin/pages/consult/detail/[id].tsx
@@ -690,20 +690,23 @@ export default function ConsultDetail() {
                                           <Tfee>과정 금액</Tfee>
                                         </TableRow>
                                       </Theader>
-                                      {subjectList?.result.map(
-                                        (item, index) => (
-                                          <TableItem key={index}>
-                                            <TableRow>
-                                              <Checkbox
-                                                key={item.id}
-                                                value={item.subjectName}
-                                              >
-                                                <SubjectItem tableData={item} />
-                                              </Checkbox>
-                                            </TableRow>
-                                          </TableItem>
-                                        ),
-                                      )}
+                                      {subjectList?.result !== null &&
+                                        subjectList?.result.map(
+                                          (item, index) => (
+                                            <TableItem key={index}>
+                                              <TableRow>
+                                                <Checkbox
+                                                  key={item.id}
+                                                  value={item.subjectName}
+                                                >
+                                                  <SubjectItem
+                                                    tableData={item}
+                                                  />
+                                                </Checkbox>
+                                              </TableRow>
+                                            </TableItem>
+                                          ),
+                                        )}
                                     </CheckboxGroup>
                                   </ScrollShadow>
                                   {subjectList?.totalCount > 0 && (
