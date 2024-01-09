ca012ad9f503878fa91583119fd21ee7e43d2f80
hyelim <otter4913@gmail.com>
2024-01-09 17:06:25 +0900

    스크립트

--- packages/hc-design/pages/_document.tsx
+++ packages/hc-design/pages/_document.tsx
@@ -6,22 +6,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-WP8Q4MBES6`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-WP8Q4MBES6', {
-            page_path: window.location.pathname,
-          });
-        `,
-            }}
-          /> */}
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
@@ -50,23 +34,6 @@ export default class MyDocument extends Document {
                 content="http://www.mysite.com/myimage.jpg"
               />
               <meta property="og:url" content="http://www.mysite.com" /> */}
-          <script src="https://www.googletagmanager.com/gtag/js?id=G-WP8Q4MBES6" />
-          <script id="google-analytics">
-            {`
-              window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-WP8Q4MBES6');
-        `}
-          </script>
-
-          <script>
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W22FF3RM');  `}
-          </script>
         </Head>
         <body>
           <noscript>
