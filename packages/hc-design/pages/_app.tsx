6ada2a34bc5b0a063fb05fa1c9021bf6f99f4375
hyelim <otter4913@gmail.com>
2024-01-09 16:06:01 +0900

    수정

--- packages/hc-design/pages/_app.tsx
+++ packages/hc-design/pages/_app.tsx
@@ -17,56 +17,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <NextUIProvider>
           <Layout>
             <Head>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=G-WP8Q4MBES6`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-WP8Q4MBES6', {
-            page_path: window.location.pathname,
-          });
-        `,
-                }}
-              />
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-              />
-              <meta
-                name="naver-site-verification"
-                content="b4b50776b3b55da6f0bf7cec11cf908a2da0cf1f"
-              />
               <title>H ACADEMY | 에이치 아카데미</title>
-              <meta
-                name="description"
-                content="모션그래픽, 웹툰, 이모티콘, 시네마4D, 에프터이펙트, 3D 블렌더, 클립스튜디오 국비지원 학원."
-              />
-              <meta property="og:type" content="website" />
-              <meta property="og:title" content="H ACADEMY | 에이치 아카데미" />
-              <meta
-                property="og:description"
-                content="모션그래픽, 웹툰, 이모티콘, 시네마4D, 에프터이펙트, 3D 블렌더, 클립스튜디오 국비지원 학원."
-              />
-              {/* <meta
-                property="og:image"
-                content="http://www.mysite.com/myimage.jpg"
-              />
-              <meta property="og:url" content="http://www.mysite.com" /> */}
             </Head>
-            <Script src="https://www.googletagmanager.com/gtag/js?id=G-WP8Q4MBES6" />
-            <Script id="google-analytics">
-              {`
-              window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-WP8Q4MBES6');
-        `}
-            </Script>
             <Component {...pageProps} />
           </Layout>
         </NextUIProvider>
