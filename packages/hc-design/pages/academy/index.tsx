569a80f31095d45e98e45e0cab2085ecb20fc5cc
hyelim <otter4913@gmail.com>
2024-03-05 17:48:21 +0900

    어드민 수정

--- packages/hc-design/pages/academy/index.tsx
+++ packages/hc-design/pages/academy/index.tsx
@@ -3,8 +3,19 @@ import Form from '@/components/Form'
 export default function academy() {
   return (
     <>
-      <section id="consult" className="pt-10 pb-16">
-        <Form />
+      <section>
+        <div className="max-w-[2000px] mx-auto my-0">
+          <img
+            src="https://highclass-image.s3.amazonaws.com/academy/hc_design/2024/page/academy_intro.png"
+            alt="emoticon"
+            className="hidden w-full wmd:block"
+          ></img>
+          <img
+            src="https://highclass-image.s3.amazonaws.com/academy/hc_design/2024/page/mo/academy_intro.png"
+            alt="emoticon"
+            className="block w-full wmd:hidden"
+          ></img>
+        </div>
       </section>
     </>
   )
