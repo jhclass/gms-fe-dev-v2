0cb5cf913c686f1a7c42aa4b4f5df01f9c59dcec
hyelim <otter4913@gmail.com>
2024-03-11 12:25:28 +0900

    통계권한 오픈

--- packages/admin/pages/statistics/layout.tsx
+++ packages/admin/pages/statistics/layout.tsx
@@ -13,9 +13,10 @@ export default function StatisticsLayout({ children }) {
   if (isCheckingLogin) {
     return null
   }
-  if (mGrade === grade.dev) {
-    return <main>{children}</main>
-  } else {
-    return <MainWrap>기능 점검으로 잠시 서비스 중단 합니다.</MainWrap>
-  }
+  // if (mGrade === grade.dev) {
+  //   return <main>{children}</main>
+  // } else {
+  //   return <MainWrap>기능 점검으로 잠시 서비스 중단 합니다.</MainWrap>
+  // }
+  return <main>{children}</main>
 }
