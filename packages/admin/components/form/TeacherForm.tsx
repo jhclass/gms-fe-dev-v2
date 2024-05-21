5fb2b2c15405582576014bb999201c980fa7634e
hyelim <otter4913@gmail.com>
2024-05-21 11:06:37 +0900

    수정

--- packages/admin/components/form/TeacherForm.tsx
+++ packages/admin/components/form/TeacherForm.tsx
@@ -62,9 +62,46 @@ const Noti = styled.p`
     color: red;
   }
 `
-const UpdateTime = styled.p`
-  span {
+const UpdateTime = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 0;
+    align-items: flex-end;
+  }
+`
+
+const UpdateCon = styled.p`
+  position: relative;
+  &:first-child {
+    padding-right: 0.4rem;
+    &:after {
+      content: '';
+      width: 0.3rem;
+      height: 1px;
+      background: #11181c;
+      position: absolute;
+      top: 50%;
+      margin-top: -0.5px;
+      right: -0.2rem;
+    }
+  }
+  > span {
     color: #555;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 0;
+    align-items: flex-end;
+    &:first-child {
+      padding-right: 0;
+      &:after {
+        display: none;
+      }
+    }
   }
 `
 const DetailDiv = styled.div`
@@ -345,9 +382,11 @@ export default function StudentsWrite({ managerId }) {
                 <span>*</span> 는 필수입력입니다.
               </Noti>
               <UpdateTime>
-                <span>최근 업데이트 :</span>
-                {managerData.lastModifiedBy} -{' '}
-                {formatDate(managerData?.updatedAt)}
+                <UpdateCon>
+                  <span>최근 업데이트 : </span>
+                  {managerData.lastModifiedBy}
+                </UpdateCon>
+                <UpdateCon>{formatDate(managerData?.updatedAt)}</UpdateCon>
               </UpdateTime>
             </TopInfo>
             <form onSubmit={handleSubmit(onSubmit)}>
