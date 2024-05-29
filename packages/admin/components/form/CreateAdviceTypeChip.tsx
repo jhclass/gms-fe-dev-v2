196f31309e4d2bb299b97dd8cbd13ee722a960a0
hyelim <otter4913@gmail.com>
2024-05-29 10:29:43 +0900

    수정

--- packages/admin/components/form/CreateAdviceTypeChip.tsx
+++ packages/admin/components/form/CreateAdviceTypeChip.tsx
@@ -15,7 +15,6 @@ export default function CreateAdviceTypeChip({
   adviceList,
   refetch,
   category,
-  totalCount,
 }) {
   const { userLogs } = useUserLogsMutation()
   const [editAdvice] = useMutation(EDIT_ADVICE_TYPE_MUTATION)
