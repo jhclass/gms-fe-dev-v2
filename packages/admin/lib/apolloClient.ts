4901165d4ec0724fbedc37064a028d772bd9e69d
Jinhyoung Lee <jhcode0@naver.com>
2024-07-03 16:07:14 +0900

    apolloClient Update

--- packages/admin/lib/apolloClient.ts
+++ packages/admin/lib/apolloClient.ts
@@ -34,11 +34,15 @@ const authLink = new ApolloLink((operation, forward) => {
 })
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
-    graphQLErrors.map(({ message, locations, path }) =>
+    graphQLErrors.forEach(({ message, locations, path }) => {
       console.error(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
-    )
+      )
+      if (message === 'UNAUTHENTICATED') {
+        localStorage.removeItem('token')
+        window.location.href = '/login'
+      }
+    })
   }
   if (networkError) console.error(`[Network error]: ${networkError}`)
 })
