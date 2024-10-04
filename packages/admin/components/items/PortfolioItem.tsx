9a021483ca8c8653da278795f3a65550cb7ed450
hyelim <otter4913@gmail.com>
2024-10-04 11:58:54 +0900

    포트폴리오 버튼 위치 수정

--- packages/admin/components/items/PortfolioItem.tsx
+++ packages/admin/components/items/PortfolioItem.tsx
@@ -27,8 +27,9 @@ const PortfolioFigure = styled.figure`
 const PortfolioBtn = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding: 0.5rem 1rem;
+  justify-content: flex-end;
+  gap: 0.5rem;
+  padding: 0.5rem;
   width: 100%;
 `
 
